881b78d1ffd3c4b6878dc3fcf7c5415dac73048f
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-29 02:38:14 +0000

    initial table done with robot pictures

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,7 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_directors = Director.all
     render({ :template => 'director_templates/index.html.erb'})
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" }) 
+
+  get("/directors/:id", { :controller => "directors", :action => "detail" }) 
 end
